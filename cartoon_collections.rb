8cf4998f047a947925b18b30b3f42d1ba843f619
Jack de Grave <jack.degrave@outlook.com>
2019-05-31 14:10:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find do |cheese|
+  array.include do |cheese|
     
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
