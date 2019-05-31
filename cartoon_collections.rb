5850be18e83ed21f76e2c7cb8f5be2ef373f06c2
Jack de Grave <jack.degrave@outlook.com>
2019-05-31 12:51:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index do |dwarf, i|
-    puts "#{i} #{dwarf}" 
+  array.each_with_index do |dwarf, index|
+    puts "#{index} #{dwarf}" 
   end  
 end
 
