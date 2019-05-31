d19bc4173eb56ae63791f9bc34ee74c751942a12
Jack de Grave <jack.degrave@outlook.com>
2019-05-31 13:01:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 def summon_captain_planet(array)
   array.each do |calls|
     calls.capitalize
-    string = array.join("!")
-    string.slice
+    string = array.join("! ")
+    array = string.split
   end
   array
 end
